6bb12981da3829f0626271b57cf4dcbdec344592
Kevin Walton <notkevinwalton@gmail.com>
2022-07-26 09:30:30 -0400

    commit everything from old repo

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# minecraft_server
+# Server Info
+**IP:** waltoncloud.ddns.net
+
+**Port:** 19132 (default)
+
+# Server Maintenance & Backups
+The server will reboot for maintenance and to back up the world files on Mondays, Wednesdays, and Fridays at 4am EST. Being logged in during this time will result in being kicked from the server, and is probably a bad idea.
+
+
+# Installation Notes
+The `minecraft_bedrock.sh` script includes logic to save changes and push to this repo when started and is designed to be run on server reboot. The `minecraft_bedrock` executable file is not included with this repo and can be downloaded from [minecraft.net](https://www.minecraft.net/en-us/download/server/bedrock).
+
+### Command Line Example:
+```
+# Running with backup script
+sudo /opt/minecraft/minecraft_bedrock.sh
+
+# Just starting the server executable
+sudo LD_LIBRARY_PATH=/opt/minecraft /opt/minecraft/bedrock_server
+```
+
+### Cronjob Example (running as sudo):
+```
+@reboot /opt/minecraft/minecraft_bedrock.sh > /opt/minecraft/server_console.log
+```
---
+++ minecraft_bedrock.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+##################################################################
+# Notes: This script is run as sudo by crontab on server reboot. #
+# It will push a backup the world, then start the server.        #
+# All directory references are relative to this script.          #
+##################################################################
+BEDROCK_SERVER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+LD_LIBRARY_PATH="$BEDROCK_SERVER_DIR"
+cd $BEDROCK_SERVER_DIR
+
+######## Wait for networking stuff to come online after reboot ########
+echo "Waiting 60 seconds for network..."
+sleep 60
+
+######## Run Server GitHub Backup ########
+# Create commit on all files with timestamp as message
+CURRENT_TIME=$(date "+%Y.%m.%d-%H.%M.%S")
+echo "Committing Changes..."
+git add -A && git commit -a -m "$CURRENT_TIME"
+
+# Push to GitHub using SSH
+echo "Pushing to Github..."
+git push origin master -v
+
+######## Start Minecraft Bedrock Server ########
+echo "Starting Server..."
+cd $BEDROCK_SERVER_DIR && ./bedrock_server
