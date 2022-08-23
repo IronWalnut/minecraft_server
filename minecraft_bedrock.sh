04228123ce648fd510564e34078ceaec95ed0a63
Kevin Walton <notkevinwalton@gmail.com>
2022-08-23 18:44:27 -0400

    comments

--- minecraft_bedrock.sh
+++ minecraft_bedrock.sh
@@ -5,6 +5,8 @@
 # It will push a backup the world, then start the server.        #
 # All directory references are relative to this script.          #
 ##################################################################
+
+######## Set working dir & LD_LIBRARY_PATH to directory the script is in ########
 BEDROCK_SERVER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 LD_LIBRARY_PATH="$BEDROCK_SERVER_DIR"
 cd $BEDROCK_SERVER_DIR
@@ -23,7 +25,7 @@ git add -A && git commit -a -m "Auto-commit $CURRENT_TIME"
 echo "DONE!"
 echo
 
-# Push to GitHub using SSH and capturing stderr, track upload times
+######## Push to GitHub using SSH and capturing stderr, track upload times ########
 SECONDS=0
 git push origin main -v 2>&1
 echo "DONE! - Took $(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
