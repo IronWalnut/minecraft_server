080fd8faa2b9e78ad04280c09d841c13a75b5dac
Kevin Walton <notkevinwalton@gmail.com>
2022-09-02 11:01:12 -0400

    More auto-upgrade script

--- auto_upgrade.sh
+++ auto_upgrade.sh
@@ -11,6 +11,7 @@ EXTRACTED_DIR="$AUTO_UPGRADE_TEMP_DIR/extracted_server_files"
 echo "Enter URL for Bedrock Server download:"
 echo "(From https://www.minecraft.net/en-us/download/server/bedrock)"
 read BEDROCK_ZIP_URL
+echo
 
 # Parse file name from user input
 ZIP_FILE_NAME=$(echo "$BEDROCK_ZIP_URL" | sed 's:.*/::')
@@ -18,6 +19,8 @@ ZIP_FILE_NAME=$(echo "$BEDROCK_ZIP_URL" | sed 's:.*/::')
 # Confirm Upgrade y/n?
 echo "Got URL: $BEDROCK_ZIP_URL"
 echo "And file name: $ZIP_FILE_NAME"
+echo
+echo "WARNING: THIS IS A POTENTIALLY DESTRUCTIVE OPERATION!"
 while true; do
     read -p "Continue? (y/n)" yn
     case $yn in
@@ -37,19 +40,19 @@ echo
 # Move to BEDROCK_SERVER_DIR
 cd $BEDROCK_SERVER_DIR
 
-# # Run GitHub Backup
-# CURRENT_TIME=$(date "+%Y.%m.%d-%H.%M.%S")
-# echo "Committing changes..."
-# # Create commit on all files with timestamp as message
-# git add -A && git commit -a -m "Pre-upgrade commit $CURRENT_TIME"
-# echo "DONE!"
-# echo
+# Run GitHub Backup
+CURRENT_TIME=$(date "+%Y.%m.%d-%H.%M.%S")
+echo "Committing changes..."
+# Create commit on all files with timestamp as message
+git add -A && git commit -a -m "Pre-upgrade commit $CURRENT_TIME"
+echo "DONE!"
+echo
 
-# # Push to GitHub using SSH and capturing stderr, track upload times
-# SECONDS=0
-# git push origin main -v 2>&1
-# echo "DONE! - Took $(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
-# echo
+# Push to GitHub using SSH and capturing stderr, track upload times
+SECONDS=0
+git push origin main -v 2>&1
+echo "DONE! - Took $(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
+echo
 
 # Create & move to temp dir to download & extract new server files
 mkdir $AUTO_UPGRADE_TEMP_DIR
@@ -59,17 +62,23 @@ cd $AUTO_UPGRADE_TEMP_DIR
 echo "Downloading server zip..."
 wget $BEDROCK_ZIP_URL
 unzip "$AUTO_UPGRADE_TEMP_DIR/$ZIP_FILE_NAME" -d "$EXTRACTED_DIR"
+echo
 
 # Delete generic config files we don't want to overwrite ours with
+echo "Deleting generic config files..."
 rm "$EXTRACTED_DIR/allowlist.json" 
 rm "$EXTRACTED_DIR/permissions.json" 
 rm "$EXTRACTED_DIR/server.properties"
+echo "DONE!"
+echo
 
 # Move back to BEDROCK_SERVER_DIR & delete ALL untracked files to remove old version
 cd $BEDROCK_SERVER_DIR
-git clean -f
+git clean -dfx
 
 # Copy newly extracted files into repo
 cp -a "$EXTRACTED_DIR/." "$"
 
+# Remove temp dir
+rm -R "$AUTO_UPGRADE_TEMP_DIR"
 
